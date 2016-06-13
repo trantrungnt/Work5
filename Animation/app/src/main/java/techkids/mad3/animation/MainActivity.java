0a55a596df0a658832f15178a0d75e45418f36d5
Trần Trung <nguyentrantrung1986@gmail.com>
2016-06-13 09:08:44 +0700

    move button (temp)

---
+++ Animation/app/src/main/java/techkids/mad3/animation/MainActivity.java
@@ -0,0 +1,57 @@
+package techkids.mad3.animation;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.graphics.Point;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
+import android.view.animation.AnimationSet;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnClick;
+    private int mWidth, mHeight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initComponent();
+    }
+
+
+    private void initComponent()
+    {
+        btnClick = (Button) this.findViewById(R.id.btnClick);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        Log.d("ABC", "X: "+ String.valueOf(btnClick.getX()));
+        Log.d("ABC", "btn Width: "+ String.valueOf(btnClick.getWidth()));
+        Log.d("ABC", "screen Width: "+ String.valueOf(mWidth));
+        DisplayMetrics displayMatrics = new DisplayMetrics();
+        ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getMetrics(displayMatrics);
+        mWidth = displayMatrics.widthPixels;
+        mHeight = displayMatrics.heightPixels;
+        ObjectAnimator objectAnimatorX = ObjectAnimator.ofFloat(btnClick, "x", btnClick.getX(), mWidth - btnClick.getWidth());
+        ObjectAnimator objectAnimatorY = ObjectAnimator.ofFloat(btnClick, "y", btnClick.getX(), mWidth - btnClick.getHeight());
+        objectAnimatorX.setDuration(5000);
+        objectAnimatorY.setDuration(5000);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(objectAnimatorX, objectAnimatorY);
+        animatorSet.start();
+    }
+
+}
