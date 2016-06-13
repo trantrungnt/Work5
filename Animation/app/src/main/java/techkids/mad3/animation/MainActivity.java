7527d9260271b62dc1778af812a8a52c3dc63650
Trần Trung <nguyentrantrung1986@gmail.com>
2016-06-13 22:40:38 +0700

    shapes Button and move

--- Animation/app/src/main/java/techkids/mad3/animation/MainActivity.java
+++ Animation/app/src/main/java/techkids/mad3/animation/MainActivity.java
@@ -1,14 +1,19 @@
 package techkids.mad3.animation;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Point;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.WindowManager;
+import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -56,9 +61,41 @@ public class MainActivity extends AppCompatActivity {
         ObjectAnimator objectAnimatorY = ObjectAnimator.ofFloat(btnClick, "y", btnClick.getHeight(), relLayout.getHeight() - btnClick.getHeight());
         objectAnimatorX.setDuration(5000);
         objectAnimatorY.setDuration(5000);
+        objectAnimatorX.setRepeatMode(Animation.REVERSE);
+        objectAnimatorX.setRepeatCount(Animation.INFINITE);
+        objectAnimatorY.setRepeatMode(Animation.REVERSE);
+        objectAnimatorY.setRepeatCount(Animation.INFINITE);
+
+        objectAnimatorX.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int time = (int) animation.getCurrentPlayTime() / 1000;
+                switch (time) {
+                    case 1:
+                        btnClick.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.btn_cricle));
+                        break;
+                    case 2:
+                        btnClick.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.btn_parallelogram));
+                        break;
+                    case 3:
+                        btnClick.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.btn_diamond));
+                        break;
+                    case 4:
+                        btnClick.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.btn_triangle));
+                        break;
+                    default:
+                        btnClick.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.btn_rectangle));
+                        break;
+                }
+            }
+        });
+
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.playTogether(objectAnimatorX, objectAnimatorY);
         animatorSet.start();
     }
 
 }
+
+
+
